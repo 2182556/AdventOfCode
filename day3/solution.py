736b4043e12832bf86715b9ae86f35136a7d96b7
2182556 <je.boellaard@student.avans.nl>
2022-12-03 12:07:02 +0100

    small changes day3

--- day3/solution.py
+++ day3/solution.py
@@ -1,17 +1,7 @@
-prioritySum, prioritySumBadges = 0, 0;
 def getPriority(c):
-    if c.isupper(): return ord(c) - ord('A') + 27;                    
-    else: return ord(c) - ord('a') + 1;
+    return ord(c) - ord('A') + 27 if c.isupper() else ord(c) - ord('a') + 1;
     
 with open('input.txt') as f:
-    for l in f:
-        commons = [c for c in l[:len(l)//2] if c in l[len(l)//2:]]
-        prioritySum += getPriority(commons[0]);
-    f.seek(0);
-    for l in f:
-        bag2, bag3 = next(f), next(f);
-        commons = [c for c in l if c in bag2 and c in bag3];  
-        prioritySumBadges += getPriority(commons[0]);
-            
-print('Solution part one: ', prioritySum);
-print('Solution part two: ', prioritySumBadges);
+    bags = f.readlines();
+    print('Solution part one: ', sum([getPriority([c for c in bag[:len(bag)//2] if c in bag[len(bag)//2:]][0]) for bag in bags]));
+    print('Solution part two: ', sum([getPriority([c for c in bags[i] if c in bags[i+1] and c in bags[i+2]][0]) for i in range(0, len(bags), 3)]));
