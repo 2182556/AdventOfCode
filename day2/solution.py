584adc17b58ee48f6890cc1a2a9adc1e0e9111b1
2182556 <je.boellaard@student.avans.nl>
2022-12-03 11:14:19 +0100

    day2

---
+++ day2/solution.py
@@ -0,0 +1,24 @@
+scorePartOne, scorePartTwo = 0;
+# Rock, paper, scissors
+# Part 2: lose, draw, win
+points = [3, 0, 6];
+with open('input.txt') as f:
+    for line in f:
+        opp = ord(line[0]) - ord('A') + 1;
+        self = ord(line[2]) - ord('X') + 1;
+
+        # part one
+        scorePartOne += self + points[(opp - self) % 3];
+            
+        # part two
+        if (self == 1): 
+            if (opp > 1): scorePartTwo += opp - 1;
+            else: scorePartTwo += 3;
+        elif (self == 2): scorePartTwo += 3 + opp;
+        else:
+            scorePartTwo += 6;
+            if (opp < 3): scorePartTwo += opp + 1;
+            else: scorePartTwo += 1;
+            
+print("Solution part one: ", scorePartOne);
+print("Solution part two: ", scorePartTwo);
