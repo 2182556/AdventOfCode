e03fa2f6d4c6291d7efe48e31b31d6ae0513091e
Joy <je.boellaard@student.avans.nl>
2024-12-04 00:08:39 +0100

    Solve 2024 day3

---
+++ 2024/day3/day3.m
@@ -0,0 +1,29 @@
+clc 
+clearvars
+
+inputFile = fopen('input.txt','r');
+input = fscanf(inputFile,'%s',[1 Inf]);
+fclose(inputFile);
+
+lines = {strcat('do()', input)};
+
+partTwo = true;
+if partTwo
+    commandoPattern = regexpPattern("(do\(\))((.|\n)*?)(don't\(\))");
+    lines = extract(lines, commandoPattern);
+end
+
+multiplications = 0;
+mulPattern = regexpPattern('mul[(][0-9]+,[0-9]+[)]');
+
+for i = 1:length(lines)
+    multiplication = extract(lines(i), mulPattern);
+    if ~isempty(multiplication)
+        for j = 1:length(multiplication)
+            numbers = str2double(extract(multiplication{j,1}, digitsPattern));
+            multiplications = multiplications + numbers(1) * numbers(2);
+        end
+    end
+end
+
+fprintf('Solution: %d\n',multiplications);
